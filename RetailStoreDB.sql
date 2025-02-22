5a82e1be8f911facb4879ab533094314d78b03b6
AGhaith <a.wesam2300@nu.edu.eg>
2025-02-22 13:19:07 +0200

    Adding Comments and Finished The First Join

--- RetailStoreDB.sql
+++ RetailStoreDB.sql
@@ -1,5 +1,6 @@
+--Creating Database--
 CREATE DATABASE RetailStoreDB;
-
+--Creating Customers Table--
 CREATE TABLE Customers
 (
 	CustomerID int IDENTITY(1,1) NOT NULL,
@@ -9,7 +10,7 @@ CREATE TABLE Customers
 	RegistrationDate datetime,
 	PRIMARY KEY(CustomerID)
 );
-
+--Creating Products Table--
 CREATE TABLE Products
 (
 	ProductID int IDENTITY(1,1) NOT NULL,
@@ -19,7 +20,7 @@ CREATE TABLE Products
 	StockQuantity int,
 	PRIMARY KEY(ProductID)
 );
-
+--Creating Orders Table--
 CREATE TABLE Orders
 (
 	OrderID int IDENTITY(1,1) NOT NULL,
@@ -29,6 +30,7 @@ CREATE TABLE Orders
 	CustomerID int FOREIGN KEY REFERENCES Customers(CustomerID),
 	ProductID int FOREIGN KEY REFERENCES Products(ProductID)
 );
+--Inserting In Customers--
 INSERT INTO Customers
 	(FirstName,LastName,Email,RegistrationDate)
 VALUES('Ahmed', 'Ghaith', 'a.wesam2300@nu.edu.eg', '2025-2-18 14:30:00'),
@@ -37,6 +39,7 @@ VALUES('Ahmed', 'Ghaith', 'a.wesam2300@nu.edu.eg', '2025-2-18 14:30:00'),
 	('Third', 'Example', 'example3@nu.edu.eg', '2025-2-21 16:00:30'),
 	('Fourth', 'Example', 'example4@nu.edu.eg', '2025-2-22 17:30:00'),
 	('Fifth', 'Example', 'example5@nu.edu.eg', '2025-2-23 18:20:45');
+--Inserting In Products--
 INSERT INTO Products
 	(ProductName,Category,StockQuantity,Price)
 VALUES
@@ -46,6 +49,7 @@ VALUES
 	('Bread' , 'DAIRY'  , 100, 2),
 	('Tuna'  , 'FISH'   , 70 , 6),
 	('Water' , 'SEAFOOD', 2  , 7);
+--Inserting In Orders--
 INSERT INTO Orders
 	(OrderDate,Quantity,CustomerID,ProductID)
 VALUES
@@ -55,26 +59,31 @@ VALUES
 	('2025-2-21 16:00:30', 1, 2, 6),
 	('2025-2-22 17:30:00', 5, 3, 4),
 	('2025-2-23 18:20:45', 9, 5, 1);
-
+--Viewing Content Of All Tables--
 SELECT *
 FROM Customers;
 SELECT *
 FROM Products;
 SELECT *
 FROM Orders;
-
+--Showing Customers That Have Registered After A Certain Time--
 SELECT *
 FROM Customers
 WHERE RegistrationDate > '2025-2-21 00:00:00';
+--Showing The Items That Have The Highest 3 Prices in Products--
 SELECT TOP 3
 	*
 FROM Products
 ORDER BY Price DESC;
-
-SELECT *, Customers.FirstName, Customers.LastName
+--Showing The Orders With The Customer's Name, Product Name, Quantity, And Order Date--
+SELECT OrderID, Customers.FirstName, Customers.LastName, Products.ProductName, Orders.Quantity, OrderDate
 FROM Orders
-	LEFT JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+	JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+	JOIN Products ON Products.ProductID = Orders.ProductID;
+--Showing Total Amount Spent By Each Customer--
 
+
+--Deleting All Tables--
 DROP TABLE Customers;
 DROP TABLE Products;
 DROP TABLE Orders;
